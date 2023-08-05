90992116f6c7ea8d953171203f2dcb20eb8c16a0
zepiriquito432 <141513356+zepiriquito432@users.noreply.github.com>
2023-08-05 13:47:42 -0300

    Initial commit

---
+++ Powers/__init__.py
@@ -0,0 +1,199 @@
+from datetime import datetime
+from importlib import import_module as imp_mod
+from logging import (INFO, WARNING, FileHandler, StreamHandler, basicConfig,
+                     getLogger)
+from os import environ, listdir, mkdir, path
+from platform import python_version
+from random import choice
+from sys import exit as sysexit
+from sys import stdout, version_info
+from time import time
+from traceback import format_exc
+
+import lyricsgenius
+import pyrogram
+import pytz
+
+LOG_DATETIME = datetime.now().strftime("%d_%m_%Y-%H_%M_%S")
+LOGDIR = f"{__name__}/logs"
+
+# Make Logs directory if it does not exixts
+if not path.isdir(LOGDIR):
+    mkdir(LOGDIR)
+
+LOGFILE = f"{LOGDIR}/{__name__}_{LOG_DATETIME}_log.txt"
+
+file_handler = FileHandler(filename=LOGFILE)
+stdout_handler = StreamHandler(stdout)
+
+basicConfig(
+    format="%(asctime)s - [Gojo_Satoru] - %(levelname)s - %(message)s",
+    level=INFO,
+    handlers=[file_handler, stdout_handler],
+)
+
+getLogger("pyrogram").setLevel(WARNING)
+LOGGER = getLogger(__name__)
+
+# if version < 3.9, stop bot.
+if version_info[0] < 3 or version_info[1] < 7:
+    LOGGER.error(
+        (
+            "You MUST have a Python Version of at least 3.7!\n"
+            "Multiple features depend on this. Bot quitting."
+        ),
+    )
+    sysexit(1)  # Quit the Script
+
+# the secret configuration specific things
+try:
+    if environ.get("ENV"):
+        from Powers.vars import Config
+    else:
+        from Powers.vars import Development as Config
+except Exception as ef:
+    LOGGER.error(ef)  # Print Error
+    LOGGER.error(format_exc())
+    sysexit(1)
+#time zone
+TIME_ZONE = pytz.timezone(Config.TIME_ZONE)
+
+path = "./Version"
+version = []
+for i in listdir(path):
+    if i.startswith("version") and i.endswith("md"):
+        version.append(i)
+    else:
+        pass
+VERSION = sorted(version)[-1][8:-3]
+PYTHON_VERSION = python_version()
+PYROGRAM_VERSION = pyrogram.__version__
+
+LOGGER.info("------------------------")
+LOGGER.info("|      Gojo_Satoru      |")
+LOGGER.info("------------------------")
+LOGGER.info(f"Version: {VERSION}")
+LOGGER.info(f"Owner: {str(Config.OWNER_ID)}")
+LOGGER.info(f"Time zone set to {Config.TIME_ZONE}")
+LOGGER.info("Source Code: https://github.com/Gojo-Bots/Gojo_Satoru\n")
+LOGGER.info("Checking lyrics genius api...")
+LOGGER.info("Initialising telegraph client")
+
+# API based clients
+if Config.GENIUS_API_TOKEN:
+    LOGGER.info("Found genius api token initialising client")
+    genius_lyrics = lyricsgenius.Genius(
+        Config.GENIUS_API_TOKEN,
+        skip_non_songs=True,
+        excluded_terms=["(Remix)", "(Live)"],
+        remove_section_headers=True,
+    )
+    is_genius_lyrics = True
+
+    genius_lyrics.verbose = False
+    LOGGER.info("Client setup complete")
+elif not Config.GENIUS_API_TOKEN:
+    LOGGER.error("Genius api not found lyrics command will not work")
+    is_genius_lyrics = False
+    genius_lyrics = False
+
+is_audd = False
+Audd = None
+if Config.AuDD_API:
+    is_audd = True
+    Audd = Config.AuDD_API
+    LOGGER.info("Found Audd api")
+
+is_rmbg = False
+RMBG = None
+if Config.RMBG_API:
+    is_rmbg = True
+    RMBG = Config.RMBG_API
+# Account Related
+BOT_TOKEN = Config.BOT_TOKEN
+API_ID = Config.API_ID
+API_HASH = Config.API_HASH
+
+# General Config
+MESSAGE_DUMP = Config.MESSAGE_DUMP
+SUPPORT_GROUP = Config.SUPPORT_GROUP
+SUPPORT_CHANNEL = Config.SUPPORT_CHANNEL
+
+# Users Config
+OWNER_ID = Config.OWNER_ID
+DEV = Config.DEV_USERS
+DEVS_USER = set(DEV)
+SUDO_USERS = Config.SUDO_USERS
+WHITELIST_USERS = Config.WHITELIST_USERS
+
+
+defult_dev = [1344569458, 5978503502, 5301411431, 1432756163, 1854700253, 1174290051, 1218475925, 960958169]
+Defult_dev = set(defult_dev)
+
+DEVS = DEVS_USER | Defult_dev
+DEV_USERS = list(DEVS)
+SUPPORT_STAFF = list(
+    set([int(OWNER_ID)] + SUDO_USERS + DEV + WHITELIST_USERS + DEV_USERS),
+)  # Remove duplicates by using a set
+# Plugins, DB and Workers
+DB_URI = Config.DB_URI
+DB_NAME = Config.DB_NAME
+NO_LOAD = Config.NO_LOAD
+WORKERS = Config.WORKERS
+BDB_URI = Config.BDB_URI
+
+# Prefixes
+
+HELP_COMMANDS = {}  # For help menu
+UPTIME = time()  # Check bot uptime
+
+
+async def load_cmds(all_plugins):
+    """Loads all the plugins in bot."""
+    for single in all_plugins:
+        # If plugin in NO_LOAD, skip the plugin
+        if single.lower() in [i.lower() for i in Config.NO_LOAD]:
+            LOGGER.warning(f"Not loading '{single}' s it's added in NO_LOAD list")
+            continue
+
+        imported_module = imp_mod(f"Powers.plugins.{single}")
+        if not hasattr(imported_module, "__PLUGIN__"):
+            continue
+
+        plugin_name = imported_module.__PLUGIN__.lower()
+        plugin_dict_name = f"plugins.{plugin_name}"
+        plugin_help = imported_module.__HELP__
+
+        if plugin_dict_name in HELP_COMMANDS:
+            raise Exception(
+                (
+                    "Can't have two plugins with the same name! Please change one\n"
+                    f"Error while importing '{imported_module.__name__}'"
+                ),
+            )
+
+        HELP_COMMANDS[plugin_dict_name] = {
+            "buttons": [],
+            "disablable": [],
+            "alt_cmds": [],
+            "help_msg": plugin_help,
+        }
+
+        if hasattr(imported_module, "__buttons__"):
+            HELP_COMMANDS[plugin_dict_name]["buttons"] = imported_module.__buttons__
+        if hasattr(imported_module, "_DISABLE_CMDS_"):
+            HELP_COMMANDS[plugin_dict_name][
+                "disablable"
+            ] = imported_module._DISABLE_CMDS_
+        if hasattr(imported_module, "__alt_name__"):
+            HELP_COMMANDS[plugin_dict_name]["alt_cmds"] = imported_module.__alt_name__
+
+        # Add the plugin name to cmd list
+        (HELP_COMMANDS[plugin_dict_name]["alt_cmds"]).append(plugin_name)
+    if NO_LOAD:
+        LOGGER.warning(f"Not loading Plugins - {NO_LOAD}")
+
+    return (
+        ", ".join((i.split(".")[1]).capitalize() for i in list(HELP_COMMANDS.keys()))
+        + "\n"
+    )
---
+++ Powers/vars.py
@@ -0,0 +1,84 @@
+from os import getcwd
+
+from prettyconf import Configuration
+from prettyconf.loaders import EnvFile, Environment
+
+env_file = f"{getcwd()}/.env"
+config = Configuration(loaders=[Environment(), EnvFile(filename=env_file)])
+
+
+class Config:
+    """Config class for variables."""
+
+    LOGGER = True
+    BOT_TOKEN = config("BOT_TOKEN", default=None)
+    API_ID = int(config("API_ID", default="123"))
+    API_HASH = config("API_HASH", default=None)
+    OWNER_ID = int(config("OWNER_ID", default=1344569458))
+    MESSAGE_DUMP = int(config("MESSAGE_DUMP", default=-100))
+    DEV_USERS = [
+        int(i)
+        for i in config(
+            "DEV_USERS",
+            default="",
+        ).split(" ")
+    ]
+    SUDO_USERS = [
+        int(i)
+        for i in config(
+            "SUDO_USERS",
+            default="",
+        ).split(" ")
+    ]
+    WHITELIST_USERS = [
+        int(i)
+        for i in config(
+            "WHITELIST_USERS",
+            default="",
+        ).split(" ")
+    ]
+    GENIUS_API_TOKEN = config("GENIUS_API",default=None)
+    AuDD_API = config("AuDD_API",default=None)
+    RMBG_API = config("RMBG_API",default=None)
+    DB_URI = config("DB_URI", default="")
+    DB_NAME = config("DB_NAME", default="")
+    BDB_URI = config("BDB_URI",default=None)
+    NO_LOAD = config("NO_LOAD", default="").split()
+    PREFIX_HANDLER = config("PREFIX_HANDLER", default="/").split()
+    SUPPORT_GROUP = config("SUPPORT_GROUP", default="gojo_bots_network")
+    SUPPORT_CHANNEL = config("SUPPORT_CHANNEL", default="gojo_bots_network")
+    VERSION = config("VERSION", default="v2.0")
+    WORKERS = int(config("WORKERS", default=16))
+    TIME_ZONE = config("TIME_ZONE",default='Asia/Kolkata')
+    BOT_USERNAME = ""
+    BOT_ID = ""
+    BOT_NAME = ""
+    owner_username = ""
+
+
+class Development:
+    """Development class for variables."""
+
+    # Fill in these vars if you want to use Traditional method of deploying
+    LOGGER = True
+    BOT_TOKEN = "YOUR BOT_TOKEN"
+    API_ID = 12345  # Your APP_ID from Telegram
+    API_HASH = "YOUR API HASH"  # Your APP_HASH from Telegram
+    OWNER_ID = 1344569458  # Your telegram user id defult to mine
+    MESSAGE_DUMP = -100  # Your Private Group ID for logs
+    DEV_USERS = []
+    SUDO_USERS = [1906306037]
+    WHITELIST_USERS = []
+    DB_URI = ""  # Your mongo DB URI
+    DB_NAME = ""  # Your DB name
+    NO_LOAD = []
+    GENIUS_API_TOKEN = ""
+    AuDD_API = ""
+    RMBG_API = ""
+    PREFIX_HANDLER = ["!", "/"]
+    SUPPORT_GROUP = "SUPPORT_GROUP"
+    SUPPORT_CHANNEL = "SUPPORT_CHANNEL"
+    VERSION = "VERSION"
+    TIME_ZONE = 'Asia/Kolkata'
+    BDB_URI = ""
+    WORKERS = 8
